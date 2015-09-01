b1e33c322a71833a8c7212600b4cec6a00c62321
Mitsuki Miyagi <mir7367@gmail.com>
2015-09-02 04:15:21 +0900

    update

--- parking.cpp
+++ parking.cpp
@@ -7,14 +7,18 @@ extern "C"
 #include "kernel.h"
 #include "kernel_id.h"
 #include "ecrobot_interface.h"
+#define MOTOR_B 0
+#define MOTOR_C 1
+    
     class Parking{
     public:
-        int nrotate;
-        int rotate;
+        int nrotate; //現在の回転数
+        int srotate; //初期の回転数
         
         sensor sensor;
         Cal cal;
         
+        //前輪の回転数をリセットする
         void reset(int speed){
             while(1){
                 nrotate = nxt_motor_get_count(PORT_A);
@@ -31,14 +35,15 @@ extern "C"
             }
         }
         
+        //前輪を回転させる
         void angle(int number,int speed){
-            rotate = nxt_motor_get_count(PORT_A);
+            srotate = nxt_motor_get_count(PORT_A);
             while(1){
                 nrotate = nxt_motor_get_count(PORT_A);
-                if (nrotate < (rotate + number)){
+                if (nrotate < (srotate + number)){
                     motorA.setPWM(speed);
                 }
-                else if (nrotate > (rotate + number)){
+                else if (nrotate > (srotate + number)){
                     motorA.setPWM((-1) * speed);
                 }
                 else {
@@ -47,23 +52,25 @@ extern "C"
                 }
             }
         }
+        
+        //後輪を回転させる
         void forward(int number,int lspeed,int rspeed,int tire){
-            if (tire == 0){
-                rotate = nxt_motor_get_count(PORT_B);
-            } else if (tire == 1) {
-                rotate = nxt_motor_get_count(PORT_C);
+            if (tire == MOTOR_B){
+                srotate = nxt_motor_get_count(PORT_B);
+            } else if (tire == MOTOR_C) {
+                srotate = nxt_motor_get_count(PORT_C);
             }
             while(1){
-                if (tire == 0){
+                if (tire == MOTOR_B){
                     nrotate = nxt_motor_get_count(PORT_B);
-                } else if (tire == 1) {
+                } else if (tire == MOTOR_C) {
                     nrotate = nxt_motor_get_count(PORT_C);
                 }
-                if (nrotate > (rotate + ((-1) * number))){
+                if (nrotate > (srotate + ((-1) * number))){
                     motorB.setPWM((-1) * lspeed);
                     motorC.setPWM((-1) * rspeed);
                 }
-                else if (nrotate < (rotate + ((-1) * number))){
+                else if (nrotate < (srotate + ((-1) * number))){
                     motorB.setPWM(lspeed);
                     motorC.setPWM(rspeed);
                 }
@@ -75,7 +82,8 @@ extern "C"
             }
         }
         
-        void black(int lspeed,int rspeed){
+        //黒判定により後輪を回転させる
+        void bforward(int lspeed,int rspeed){
             while(1){
                 int now = sensor.nowlight(0);
                 int ava = sensor.ret_avarage();
@@ -97,23 +105,23 @@ extern "C"
             while(1)
             {
                 reset(100);
-                forward(100,40,40,1);
+                forward(100,40,40,MOTOR_C);
                 //前輪角度
                 angle(700,100);
                 //回転
-                forward(290,80,0,0);
+                forward(290,80,0,MOTOR_B);
                 
                 //角度直し
                 reset(100);
                 //バック
-                forward(-780,40,40,1);
+                forward(-780,40,40,MOTOR_C);
                 clock.wait(5000);
                 
-                black(40,40);
-                forward(130,40,40,1);
+                bforward(40,40);
+                forward(130,40,40,MOTOR_C);
                 
                 angle(-700,100);
-                black(0,80);
+                bforward(0,80);
                 reset(100);
                 
                 clock.wait(1000);
@@ -138,16 +146,16 @@ extern "C"
             {
                 reset(100);
                 
-                forward(450,40,40,1);
+                forward(450,40,40,MOTOR_C);
                 
                 angle(350,100);
-                forward(310,40,40,1);
+                forward(310,40,40,MOTOR_C);
                 reset(100);
                 
-                forward(-820,40,40,1);
+                forward(-820,40,40,MOTOR_C);
                 
                 angle(-650,100);
-                forward(120,0,70,1);
+                forward(120,0,70,MOTOR_C);
                 reset(100);
                 
                 clock.wait(5000);
@@ -155,14 +163,14 @@ extern "C"
                 
                 //sensor
                 angle(680,100);
-                forward(290,80,0,0);
+                forward(290,80,0,MOTOR_B);
                 reset(100);
-                black(40,40);
+                bforward(40,40);
                 
-                forward(90,40,40,1);
+                forward(90,40,40,MOTOR_C);
                 
                 angle(-700,100);
-                black(0,80);
+                bforward(0,80);
                 reset(100);
                 
                 clock.wait(1000);
