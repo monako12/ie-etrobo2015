5e686534a183dc7f9bdad8217c0112e9dddf0d9c
e135754 <e135754@ie.u-ryukyu.ac.jp>
2015-09-06 23:05:14 +0900

    make class "unknown"

---
+++ unknown.cpp
@@ -0,0 +1,16 @@
+using namespace ecrobot;
+extern "C"
+{
+    class Unknown{
+        public:
+            int map[5][6];
+            int start_pos[2];
+
+        void Check_barcode(int array[9]){
+
+        }
+        void Make_map(int array[8]){}
+
+
+    }
+}
