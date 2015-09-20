72e62fe8f26df7ad2ca16d6aa546e33bcf908fd7
monako12 <monako1201@gmail.com>
2015-09-20 19:36:57 +0900

    nihonbashi
    nihonbashi

--- Nihonbashi.cpp
+++ Nihonbashi.cpp
@@ -1,26 +1,34 @@
 extern "C"
 {
-	//Barcode bar;
 	class Nihonbashi{
     int nowl;
     int ret_pid;
     int line;
   public:
 
-    
+
     void nihon();
     void stop_ex();
   };
 
-void Nihonbashi::nihon()
-{
+  void Nihonbashi::nihon()
+  {
+   motorB.setPWM(100);
+   motorC.setPWM(100);
+   clock.wait(300);
+   motorC.setPWM(-100);
+   motorB.setPWM(-100);
+   clock.wait(700);
+   motorB.setPWM(-70);
+   motorC.setPWM(-70);
 
 
-}
+ }
 
-void Nihonbashi::stop_ex()
-{
-  //	Barcode.search_bord();
+ void Nihonbashi::stop_ex()
+ {
+  bar.search_bord();
+  nihon();
 }
 
 
