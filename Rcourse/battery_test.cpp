856917009f95fe437364536a391c2ffc391cdc4c
Ryusuke1015 <e135733@ie.u-ryukyu.ac.jp>
2015-12-22 11:27:53 +0900

    caommit add battery_test.cpp

--- Rcourse/battery_test.cpp
+++ Rcourse/battery_test.cpp
@@ -33,39 +33,39 @@ extern "C"
         int v2 = 6800;
         int v3 = 6700;
 
-          if(battery > v1){
-            motorB.setPWM(50);
-            motorC.setPWM(50);
-            motorA.setPWM(50);
+              if(battery > v1){
+                motorB.setPWM(50);
+                motorC.setPWM(50);
+                motorA.setPWM(50);
+              }
+              else if(battery > v2){
+                motorB.setPWM(30);
+                motorC.setPWM(30);
+                motorA.setPWM(30);
+              }
+              else if(battery > v3){
+                motorB.setPWM(20);
+                motorC.setPWM(20);
+                motorA.setPWM(20);
+              }
+              else{
+                motorB.setPWM(0);
+                motorC.setPWM(0);
+                motorA.setPWM(0);
+              }
           }
-          else if(battery > v2){
-            motorB.setPWM(30);
-            motorC.setPWM(30);
-            motorA.setPWM(30);
+
+          int battery_main(){   //電圧を画面に出力
+            battery = ecrobot_get_battery_voltage();
+
+            motor_battery_pro();
+
+            lcd.clear();
+            lcd.putf("sdn","voltage[mV]:",battery,0);
+            lcd.disp();
+            clock.wait(500);
           }
-          else if(battery > v3){
-            motorB.setPWM(20);
-            motorC.setPWM(20);
-            motorA.setPWM(20);
-          }
-          else{
-            motorB.setPWM(0);
-            motorC.setPWM(0);
-            motorA.setPWM(0);
-          }
-      }
-
-      int battery_main(){   //電圧を画面に出力
-        battery = ecrobot_get_battery_voltage();
-
-        motor_battery_pro();
-
-        lcd.clear();
-        lcd.putf("sdn","voltage[mV]:",battery,0);
-        lcd.disp();
-        clock.wait(500);
-      }
 
 
-  };
-}
+      };
+    }
