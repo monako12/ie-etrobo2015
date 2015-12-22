355f2ea30bc218f744477be8deee7f5731081f60
Ryusuke1015 <e135733@ie.u-ryukyu.ac.jp>
2015-12-22 17:17:45 +0900

    Merge branch 'master' of https://github.com/monako12/ie-etrobo2015 into suzuki
    # Conflicts:
    #	Lcourse/barcode.cpp
    #	Rcourse/barcode.cpp
    #	Rcourse/black.cpp

--- Rcourse/battery_test.cpp
+++ Rcourse/battery_test.cpp
@@ -10,10 +10,19 @@ extern "C"
 
   class Battery{
     public:
-      int battery;
+        int battery_main();
+        int get_voltage(){return(ecrobot_get_battery_voltage());};
 
-      void motor_battery(){  //電圧が7000以下なら停止する関数
-        int v = 7000;
+    private:
+        int  battery;
+        void motor_battery();
+        void motor_battery_pro();
+        void fix_voltage(int adjust);//目標値を引数として渡す
+  };
+
+
+      void Battery::motor_battery(){ //電圧が7000以下なら停止する関数
+        const int v = 7000;
 
         if(battery > v){
           motorB.setPWM(30);
@@ -28,44 +37,61 @@ extern "C"
         }
       }
 
-      void motor_battery_pro(){//電圧によって速度を変化させる
-        int v1 = 6900;
-        int v2 = 6800;
-        int v3 = 6700;
+      void Battery::motor_battery_pro(){//電圧によって速度を変化させる
+        const int v1 = 6900;
+        const int v2 = 6800;
+        const int v3 = 6700;
 
-              if(battery > v1){
-                motorB.setPWM(50);
-                motorC.setPWM(50);
-                motorA.setPWM(50);
-              }
-              else if(battery > v2){
+          if(battery > v1){
+            motorB.setPWM(50);
+            motorC.setPWM(50);
+            motorA.setPWM(50);
+          }
+          else if(battery > v2){
+            motorB.setPWM(30);
+            motorC.setPWM(30);
+            motorA.setPWM(30);
+          }
+          else if(battery > v3){
+            motorB.setPWM(20);
+            motorC.setPWM(20);
+            motorA.setPWM(20);
+          }
+          else{
+            motorB.setPWM(0);
+            motorC.setPWM(0);
+            motorA.setPWM(0);
+          }
+      }
+
+      void Battery::fix_voltage(int adjust){
+        const int confirm = 10;
+              int count   = 0;
+
+        while(count <= confirm){ //adjust以下の値を10回確認したら終了。
+            if(battery <= adjust){
+                count++;
+            }
+            else{
                 motorB.setPWM(30);
                 motorC.setPWM(30);
                 motorA.setPWM(30);
-              }
-              else if(battery > v3){
-                motorB.setPWM(20);
-                motorC.setPWM(20);
-                motorA.setPWM(20);
-              }
-              else{
-                motorB.setPWM(0);
-                motorC.setPWM(0);
-                motorA.setPWM(0);
-              }
-          }
+            }
+        }
+        motorB.setPWM(0);
+        motorC.setPWM(0);
+        motorA.setPWM(0);
+      }
 
-          int battery_main(){   //電圧を画面に出力
-            battery = ecrobot_get_battery_voltage();
+      int Battery::battery_main(){   //電圧を画面に出力
+        battery = get_voltage();
 
-            motor_battery_pro();
+        motor_battery_pro();
 
-            lcd.clear();
-            lcd.putf("sdn","voltage[mV]:",battery,0);
-            lcd.disp();
-            clock.wait(500);
-          }
+        lcd.clear();
+        lcd.putf("sdn","voltage[mV]:",battery,0);
+        lcd.disp();
+        clock.wait(1000);
+      }
 
-
-      };
-    }
+}
